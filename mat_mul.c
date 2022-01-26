320628e0d1face31dce84897cd04581015466cd5
akib1162100 <35798840+akib1162100@users.noreply.github.com>
2022-01-26 13:41:27 +0600

    matmul ph1

--- mat_mul.c
+++ mat_mul.c
@@ -2,10 +2,28 @@
 #include<stdlib.h>
 
 
-int** matmul(int** a,int** b)
+int** initializeZero(int dim_x,int dim_y)
 {
-    
+    int i,j;
+    int** arr1= (int**)malloc(dim_x1*sizeof(int*));
+    for (i = 0; i < dim_x1; i++)
+    {
+        arr1[i] = (int*)malloc(dim_y1*sizeof(int));
+    }
+    printf("Enter the elements of matrix :\n");
+    for (i = 0; i < dim_x1; i++)
+    {
+        for (j = 0; j < dim_y1; j++)
+        {
+            arr1[i][j] = 0;
+            // scanf("%d",&arr1[i][j]);
+        }
+    }
+
+    return arr1;
 }
+
+
 int** initialize(int dim_x,int dim_y)
 {
     int i,j;
@@ -26,6 +44,27 @@ int** initialize(int dim_x,int dim_y)
     return arr1;
 }
 
+
+
+int** matmul(int** a,int** b,int dim_x1,int dim_y2,int common_dim)
+{
+    int i,j,k;
+    int** c;
+    c = initializeZero(dim_x1,dim_y2);
+    for (i=0;i<dim_x1;i++)
+    {
+        for (j = 0; j < dim_y2; j++)
+        {
+            for(k=0 ; k<common_dim;k++)
+            {
+                // c[i][j]+=a[i][k]*b[k][j];
+                *(*(c+i)+j)+= *(*(a+i)+k)* *(*(b+k)+j);
+            }
+        }
+        
+    }
+}
+
 int main()
 {
     int dim_x1,dim_y1,dim_x2,dim_y2,i,j,k;
@@ -40,6 +79,6 @@ int main()
     }
     int** arr1=initialize(dim_x1,dim_y1);
     int** arr2=initialize(dim_x2,dim_y2);
-    int** arr3=matmul(arr1,arr2);
+    int** arr3=matmul(arr1,arr2,dim_x1,dim_y2,dim_y1);
     return 0;
 }
